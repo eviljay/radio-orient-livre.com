686e7fb9809be8c9bd24f0337bfd3400ddff2338
evgerush <evgencherkashyn@gmail.com>
2023-09-22 12:19:39 +0300

    y

---
+++ Radio_Orient_Livre/chapitre_1_files/chapitre_1.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20150129
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('chapitre_1_files/chapitre_1Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
